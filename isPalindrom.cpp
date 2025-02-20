8cd372a12edebf9c8356443732f95f91123626f2
Виктория <neyforite@MacBook-Air-Viktoria.local>
2025-02-20 22:51:57 +0700

    isPalindrom

---
+++ isPalindrom.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+std::string isPalindrom(std::string& string){
+    std::string reverse_string;
+    string.erase(std::remove(string.begin(), string.end(), ' '), string.end());
+    for(int i = string.size()-1; i >= 0; --i){
+        reverse_string += string[i];
+    }
+    return string == reverse_string? "yes" : "no";
+}
+
+int main(){
+    std::string string, result;
+    std::getline(std::cin, string);
+
+    result = isPalindrom(string);
+    std::cout << result;
+}
