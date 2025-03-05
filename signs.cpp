ecbccc4b9bfba9a8920f606772e12194ced08d58
Виктория <neyforite@MacBook-Air-Viktoria.local>
2025-03-05 10:19:02 +0700

    comment

--- signs.cpp
+++ signs.cpp
@@ -17,6 +17,6 @@ int main(){
     cout << String2 << endl;
 }
 
-int isNotSign(char String1){
+int isNotSign(char String1){ //функция знак ли
     return ((String1 >= 65 && String1 <= 90) || (String1 >= 97 && String1 <= 122) || (String1 >= 48 && String1 <= 57)) || String1 == ' ';
 }
